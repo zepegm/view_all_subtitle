bd5a12b2e6922e2e12869c2420eec9458588bcac
zepegm <59025669+zepegm@users.noreply.github.com>
2023-01-19 18:20:19 -0300

    update

--- automatic_generator.py
+++ automatic_generator.py
@@ -6,7 +6,7 @@ from powerpoint import pegarTextoSlideShow
 legenda = pegarTextoSlideShow()
 
 html = "<html>\n\t<head>\n\t\t<title>Legenda</title>\n"
-html += "\t\t<style>\n\t\t\t.text {\n\t\t\t\ttext-align: center;\n\t\t\t\tfont-family: 'Arial';\n\t\t\t\twidth: 1640px;\n\t\t\t\theight: 20vh;\n\t\t\t\tbackground: rgb(0, 0, 0, 0.8);\n\t\t\t\tcolor: white;\n\t\t\t}"
+html += "\t\t<style>\n\t\t\t.text {\n\t\t\t\ttext-align: center;\n\t\t\t\tfont-family: 'Arial';\n\t\t\t\twidth: 83vw;\n\t\t\t\theight: 20vh;\n\t\t\t\tbackground: rgb(0, 0, 0, 0.8);\n\t\t\t\tcolor: white;\n\t\t\t}"
 html += "\n\n\t\t\t.retangulo {\n\t\t\t\twidth: 100vw;\n\t\t\t\theight: 3vh;\n\t\t\t\tbackground: rgb(255, 255, 255);\n\t\t\t}\n\t\t</style>"
 html += '\n\t\t<script src="static/js/jquery-3.6.0.min.js"></script>'
 html += '\n\t\t<script src="static/js/textFit.min.js"></script>\n\t</head>'
